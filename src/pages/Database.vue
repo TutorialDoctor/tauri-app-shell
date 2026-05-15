8aa6c154e9257e74f25bbde2f9509385851a3848
tutorialdoctor <thetutorialdoctor@gmail.com>
2026-05-15 06:17:30 -0400

    sqlite db

--- src/pages/Database.vue
+++ src/pages/Database.vue
@@ -20,7 +20,7 @@ const user = ref({
 
 let db = null;
 
-function resetTitle(){
+function resetTitle() {
     buttonTitle.value = "Create User"
 }
 
@@ -151,25 +151,28 @@ onMounted(async () => {
 
 <template>
     <div class="w-4/5">
-        
+
         <div class="mt-4 flex justify-between">
-                        <div id="breadcrumbs" class="w-2/5 rounded-lg text-xs bg-gray-100 dark:bg-gray-800 p-1 gap-x-2">
-                            <button class="inline-block shadow-sm p-1 bg-white dark:bg-gray-900 rounded-md" role="tab">Items</button>
-                            <RouterLink to="detail" class="inline-block ml-4 p-1 text-gray-500 dark:text-gray-200 rounded-md" role="tab">Item</RouterLink>
-                            <RouterLink to="settings" class="inline-block ml-4 p-1 text-gray-500 dark:text-gray-200 rounded-md" role="tab">Settings</RouterLink>
+            <div id="breadcrumbs" class="w-2/5 rounded-lg text-xs bg-gray-100 dark:bg-gray-800 p-1 gap-x-2">
+                <button class="inline-block shadow-sm p-1 bg-white dark:bg-gray-900 rounded-md"
+                    role="tab">Items</button>
+                <RouterLink to="detail" class="inline-block ml-4 p-1 text-gray-500 dark:text-gray-200 rounded-md"
+                    role="tab">Item</RouterLink>
+                <RouterLink to="settings" class="inline-block ml-4 p-1 text-gray-500 dark:text-gray-200 rounded-md"
+                    role="tab">Settings</RouterLink>
 
-                        </div>
-                        <button id="cta-button" @click="deleteAllUsers()"
-                            class="text-center text-xs bg-black text-white px-4 py-2 rounded-lg flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
-                                class="bi bi-plus-circle" viewBox="0 0 16 16">
-                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                                <path
-                                    d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
-                            </svg>
+            </div>
+            <button id="cta-button" @click="deleteAllUsers()"
+                class="text-center text-xs bg-black text-white px-4 py-2 rounded-lg flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
+                    class="bi bi-plus-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                    <path
+                        d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                </svg>
 
-                            <span class="ml-2">Delete All</span></button>
-                    </div>
+                <span class="ml-2">Delete All</span></button>
+        </div>
         <!--  -->
 
         <div id="form" class="mt-4 p-4 rounded-lg border border-gray-300 shadow-md">
@@ -205,81 +208,90 @@ onMounted(async () => {
                 </div>
                 <div>
                     <button @click="createOrUpdateUser()"
-                        class="text-center text-xs bg-black text-white px-4 py-2 rounded-lg w-full">{{buttonTitle}}</button>
+                        class="text-center text-xs bg-black text-white px-4 py-2 rounded-lg w-full">{{ buttonTitle }}</button>
                 </div>
             </div>
         </div>
-    
+        
+        <div id="table" class="mt-4 col-span-2 p-4 rounded-lg border border-gray-300 shadow-md">
+            <p class="hidden text-xs font-light font-semibold">Sidebar</p>
+            <p class="hidden mt-1 text-gray-500 dark:text-gray-200" style="font-size: .70rem">Manage your cookie
+                settings here.</p>
+            <table class="w-full text-sm">
+                <thead>
+                    <tr class="border-b">
+                        <th class="h-10 px-2 text-left align-middle font-medium">
+                            <button type="button" role="checkbox" value="on"
+                                class="h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+                                aria-label="Select all"></button>
+                        </th>
+                        <th class="h-10 px-2 text-left align-middle font-medium text-muted-foreground">
+                            ID</th>
+                        <th class="h-10 px-2 text-left align-middle font-medium text-muted-foreground">
+                            <button
+                                class="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2">Name<svg
+                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" class="lucide lucide-arrow-up-down">
+                                    <path d="m21 16-4 4-4-4"></path>
+                                    <path d="M17 20V4"></path>
+                                    <path d="m3 8 4-4 4 4"></path>
+                                    <path d="M7 4v16"></path>
+                                </svg></button>
+                        </th>
+                        <th class="h-10 px-2 text-left align-middle font-medium text-muted-foreground">
+                            <div class="text-right">Email</div>
+                        </th>
+                        <th class="h-10 px-2 text-left align-middle font-medium text-muted-foreground">
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="thisUser in all_users" @click="user = thisUser"
+                        class="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted"
+                        data-state="false">
+                        <td class="p-2 align-middle">
+                            <button type="button" role="checkbox" aria-checked="false" data-state="unchecked" value="on"
+                                class="peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+                                aria-label="Select row"></button>
+                        </td>
+                        <td class="p-2 align-middle">
+                            <div class="capitalize">{{ thisUser.id }}</div>
+                        </td>
+                        <td class="p-2 align-middle">
+                            <div class="lowercase">{{ thisUser.name }}</div>
+                        </td>
+                        <td class="p-2 align-middle">
+                            <div class="text-right font-medium">{{ thisUser.email }}</div>
+                        </td>
+                        <td class="p-2 align-middle">
+                            <button @click="deleteUser(thisUser.id)" class="" type="button" id="radix-:r2o0:"
+                                aria-haspopup="menu" aria-expanded="false" data-state="closed"><span
+                                    class="sr-only">Open menu</span><svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                    height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round"
+                                    class="lucide lucide-trash-icon lucide-trash">
+                                    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
+                                    <path d="M3 6h18" />
+                                    <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+                                </svg>
+                            </button>
+                            <button @click="updateUser(thisUser.id, user.name, user.email)" class="hidden" type="button"
+                                id="radix-:r2o0:" aria-haspopup="menu" aria-expanded="false" data-state="closed"><span
+                                    class="sr-only">Open menu</span><svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                    height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round"
+                                    class="lucide lucide-rotate-cw-icon lucide-rotate-cw">
+                                    <path d="M21 12a9 9 0 1 1-9-9c2.52 0 4.93 1 6.74 2.74L21 8" />
+                                    <path d="M21 3v5h-5" />
+                                </svg>
+                            </button>
+                        </td>
 
-
-          <div id="table" class="mt-4 col-span-2 p-4 rounded-lg border border-gray-300 shadow-md">
-                <p class="hidden text-xs font-light font-semibold">Sidebar</p>
-                <p class="hidden mt-1 text-gray-500 dark:text-gray-200" style="font-size: .70rem">Manage your cookie settings here.</p>
-                <table class="w-full text-sm">
-                    <thead>
-                        <tr class="border-b">
-                            <th class="h-10 px-2 text-left align-middle font-medium">
-                                <button type="button" role="checkbox" value="on"
-                                    class="h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-                                    aria-label="Select all"></button>
-                            </th>
-                            <th class="h-10 px-2 text-left align-middle font-medium text-muted-foreground">
-                                ID</th>
-                            <th class="h-10 px-2 text-left align-middle font-medium text-muted-foreground">
-                                <button
-                                    class="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2">Name<svg
-                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round" class="lucide lucide-arrow-up-down">
-                                        <path d="m21 16-4 4-4-4"></path>
-                                        <path d="M17 20V4"></path>
-                                        <path d="m3 8 4-4 4 4"></path>
-                                        <path d="M7 4v16"></path>
-                                    </svg></button>
-                            </th>
-                            <th class="h-10 px-2 text-left align-middle font-medium text-muted-foreground">
-                                <div class="text-right">Email</div>
-                            </th>
-                            <th class="h-10 px-2 text-left align-middle font-medium text-muted-foreground">
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="thisUser in all_users" @click="user = thisUser"
-                            class="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted"
-                            data-state="false">
-                            <td class="p-2 align-middle">
-                                <button type="button" role="checkbox" aria-checked="false" data-state="unchecked"
-                                    value="on"
-                                    class="peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
-                                    aria-label="Select row"></button>
-                            </td>
-                            <td class="p-2 align-middle">
-                                <div class="capitalize">{{thisUser.id}}</div>
-                            </td>
-                            <td class="p-2 align-middle">
-                                <div class="lowercase">{{thisUser.name}}</div>
-                            </td>
-                            <td class="p-2 align-middle">
-                                <div class="text-right font-medium">{{thisUser.email}}</div>
-                            </td>
-                            <td class="p-2 align-middle">
-                                <button @click="deleteUser(thisUser.id)"
-                                    class=""
-                                    type="button" id="radix-:r2o0:" aria-haspopup="menu" aria-expanded="false"
-                                    data-state="closed"><span class="sr-only">Open menu</span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-icon lucide-trash"><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
-                                </button>
-                                 <button @click="updateUser(thisUser.id, user.name, user.email)"
-                                    class="hidden"
-                                    type="button" id="radix-:r2o0:" aria-haspopup="menu" aria-expanded="false"
-                                    data-state="closed"><span class="sr-only">Open menu</span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-rotate-cw-icon lucide-rotate-cw"><path d="M21 12a9 9 0 1 1-9-9c2.52 0 4.93 1 6.74 2.74L21 8"/><path d="M21 3v5h-5"/></svg>
-                                </button>
-                            </td>
-                               
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-                    <pre class="mt-8">{{ all_users }}</pre>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <pre class="mt-8">{{ all_users }}</pre>
     </div>
 </template>
